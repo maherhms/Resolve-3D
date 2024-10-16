d2598e7d65caa65c25ccf17200000b4cd9298ced
maherhms <maher.3000@outlook.fr>
2024-10-16 20:31:38 +0200

    Document project architecture and their components

---
+++ Resolve 3D/ResolveEditor/Utilities/Logger.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ResolveEditor.Utilities
+{
+    enum MessageType
+    {
+        Info = 0x01, Warning = 0x02, Error = 0x04,
+    }
+
+    class LogMessage
+    {
+        public DateTime Time { get; }
+        public MessageType MessageType { get; }
+        public string Message { get; }
+        public string File { get; }
+        public string Caller { get; }
+        public int Line { get; }
+        public string MetaData => $"{File}: {Caller} ({Line})";
+
+        public LogMessage(MessageType messageType, string message, string file, string caller, int line)
+        {
+            Time = DateTime.Now;
+            MessageType = messageType;
+            Message = message;
+            File = Path.GetFileName(file);
+            Caller = caller;
+            Line = line;
+        }
+    }
+    internal class Logger
+    {
+        private readonly static ObservableCollection<LogMessage> _messages = new ObservableCollection<LogMessage>();
+        public static ReadOnlyObservableCollection<LogMessage> Messages { get; } = new ReadOnlyObservableCollection<LogMessage>(_messages);
+        public static async void Log(MessageType type, string msg,
+            [CallerFilePath]string file = "", 
+            [CallerMemberName]string caller = "", 
+            [CallerLineNumber]int line=0)
+        {
+            await Application.Current.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                _messages.Add(new LogMessage(type, msg, file, caller, line));
+            }));
+        }
+    }
+}
